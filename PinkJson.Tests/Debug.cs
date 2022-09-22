dc107c1710638d74e2e17b320a0757b1ac371d4f
Vlas-Omsk <62666113+Vlas-Omsk@users.noreply.github.com>
2022-09-23 03:54:27 +0600

    #Update: Changed file structure

---
+++ PinkJson.Tests/Debug.cs
@@ -0,0 +1,36 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using PinkJson;
+using System.Threading;
+using System.Diagnostics;
+
+namespace json
+{
+    class Test
+    {
+        [JsonProperty("problems")]
+        string lol;
+    }
+
+    class Debug
+    {
+        static void Main()
+        {
+            var json = new Json(File.ReadAllText("json.json"));
+            var test = Json.ToObject<Test>(json);
+            json = Json.FromObject(test, true);
+
+            var str = json.ToFormatString();
+            Console.WriteLine(str);
+
+            Console.ReadLine();
+        }
+    }
+}
+#endif
